120ae6a8f1a9dc85041219985aeb9a1a70b183f0
Alexander Renström <alexander.renstrom@teliacompany.com>
2026-09-14 17:47:28 +0200

    updated nvim files

---
+++ lua/custom/plugins/lsp.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    'pmizio/typescript-tools.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
+    opts = {
+      on_attach = function(client, bufnr)
+        -- This is the magic line: it tells Neovim NOT to use
+        -- this specific LSP for formatting.
+        client.server_capabilities.documentFormattingProvider = false
+        client.server_capabilities.documentRangeFormattingProvider = false
+      end,
+      settings = {
+        separate_diagnostic_server = true,
+        publish_diagnostic_on = 'insert_leave',
+      },
+    },
+  },
+
+  {
+    'folke/lazydev.nvim',
+    ft = 'lua',
+    opts = {
+      library = {
+        { path = 'luvit-meta/library', words = { 'vim%.uv' } },
+      },
+    },
+  },
+}
